417681c4f7173674e745363ccb7b025c580cc906
MannanAnxari <mannananxari@gmail.com>
2023-04-26 13:21:50 +0500

    .

---
+++ app/(user)/post/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function loading() {
+    return (
+        <div className='capitalize text-1xl text-gray-800 font-bold py-24 text-center'>loading...</div>
+    )
+}
